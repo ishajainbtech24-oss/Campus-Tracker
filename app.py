4b2bfbe00ce4d4056e23894333b1fa0d7e7bcb70
Isha <ishajain2424@gmail.com>
2026-04-08 22:41:04 +0530

    prepare for railway deployment

--- app.py
+++ app.py
@@ -373,4 +373,4 @@ def admin_counters():
     return render_template('admin_counters.html', counters=counters)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
